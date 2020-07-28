e75a547e47a6eb447e633fbf2e79d56f56afb8f7
Adam Quinton <quintonish@gmail.com>
2020-07-27 19:48:12 -0600

    add ability to read thumbs directory

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const normalize = require('./lib/normalize');
 const html = require('./lib/html');
 const htmlParts = require('./views');
 const filters = require('./lib/filters');
+const thumbs = require('./lib/thumbs');
 const fileParts = require('./lib/file-name');
 const file = require('./lib/file');
 const VIDEO_PATH = require('./lib/video-path');
+const noImage = require('./lib/no-image');
 
 const SUPPORTED_VIDEO_TYPES = process.env.SUPPORTED_VIDEO_TYPES ? process.env.SUPPORTED_VIDEO_TYPES.split(',') : [
   'avi',
@@ -36,11 +38,21 @@ const routes = [
       ${htmlParts.header({ title })}
       <article>
         <ul class="movie-list">
-        ${movies.map(movie => `
+        ${movies.map(movie => {
+          const foundThumb = thumbs
+            .find(thumb => thumb.name === fileParts(movie).fileWithoutExtension);
+
+          return `
           <li class="movie-link">
-            <a href="play?movie=${movie}">${normalize(movie)}</a>
+            <a href="play?movie=${movie}">
+              ${foundThumb
+                ? `<img src="data:img/jpg;base64,${foundThumb.base64}" />`
+                : `<img src="data:img/jpg;base64,${noImage.base64}" />`}
+              <span>${normalize(movie)}</span>
+            </a>
           </li>
-          `).join('')}
+          `;
+        }).join('')}
         </ul>
       </article>
       `;
